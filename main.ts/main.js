a87a63098787fb0de32598835d1b42fa341e7e3b
jamal165165 <160552788+jamal165165@users.noreply.github.com>
2024-06-05 04:52:51 -0700

    first commit

---
+++ main.ts/main.js
@@ -0,0 +1,12 @@
+var dayoff = "monday";
+switch (dayoff) {
+    case "saturday":
+        console.log("i will rest");
+        break;
+    case "monday":
+        console.log("i read the cricket book");
+        break;
+    default:
+        console.log("Sorry");
+}
+;
---
+++ main.ts/main.ts
@@ -0,0 +1,17 @@
+
+
+let dayoff = "monday";
+
+switch  (dayoff) {
+    case "saturday":
+        console.log("i will rest");
+    break;
+    case "monday":
+        console.log("i read the cricket book");
+    break;
+    default:
+    console.log("Sorry");
+                
+};
+
+     
